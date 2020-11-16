ac779c3a3b19260445a116901acb9163834841e7
maryna kotelnikova <neveroer@gmail.com>
2020-11-16 13:03:20 +0200

    правит 2 и 3

--- js/task-2.js
+++ js/task-2.js
@@ -1,20 +1,27 @@
-const ingredients = [
-  "Картошка",
-  "Грибы",
-  "Чеснок",
-  "Помидоры",
-  "Зелень",
-  "Приправы",
-];
-// Напиши скрипт, который для каждого элемента массива ingredients создаст отдельный li,
-// после чего вставит все li за одну операцию в список ul.ingredients.
-// Для создания DOM - узлов используй document.createElement().
+// В HTML есть пустой список ul#ingredients.
 
-const listEl = document.querySelector("#ingredients");
-listEl.append(
-  ...ingredients.map((ingridient) => {
-    const liEl = document.createElement("li");
-    liEl.textContent = ingridient;
-    return liEl;
-  })
-);
+// <ul id="ingredients"></ul>
+// В JS есть массив строк.
+
+const ingredients = [
+  'Картошка',
+  'Грибы',
+  'Чеснок',
+  'Помидоры',
+  'Зелень',
+  'Приправы',
+];
+// Напиши скрипт, который для каждого элемента массива ingredients создаст отдельный li, 
+// после чего вставит все li за одну операцию в список ul.ingredients. 
+// Для создания DOM-узлов используй document.createElement().
+
+
+const listOfIngredients = document.querySelector("#ingredients");
+
+const ingredientsItem = ingredients.map((item) => {
+  const listItem = document.createElement("li");
+  listItem.textContent = item;
+  return listItem;
+});
+
+listOfIngredients.append(...ingredientsItem);
--- js/task-3.js
+++ js/task-3.js
@@ -1,33 +1,36 @@
-const images = [
-  {
-    url:
-      "https://images.pexels.com/photos/140134/pexels-photo-140134.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260",
-    alt: "White and Black Long Fur Cat",
-  },
-  {
-    url:
-      "https://images.pexels.com/photos/213399/pexels-photo-213399.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260",
-    alt: "Orange and White Koi Fish Near Yellow Koi Fish",
-  },
-  {
-    url:
-      "https://images.pexels.com/photos/219943/pexels-photo-219943.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260",
-    alt: "Group of Horses Running",
-  },
-];
+// Напиши скрипт для создания галлереи изображений по массиву данных.
 
-// Используй массив объектов images для создания тегов img вложенных в li.
+// В HTML есть список ul#gallery.
+
+// <ul id="gallery"></ul>
+//   Используй массив объектов images для создания тегов img вложенных в li.
 // Для создания разметки используй шаблонные строки и insertAdjacentHTML().
 
 // Все элементы галереи должны добавляться в DOM за одну операцию вставки.
 // Добавь минимальное оформление галереи флексбоксами или гридами через css-классы.
+const images = [
+  {
+    url:
+      'https://images.pexels.com/photos/140134/pexels-photo-140134.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260',
+    alt: 'White and Black Long Fur Cat',
+  },
+  {
+    url:
+      'https://images.pexels.com/photos/213399/pexels-photo-213399.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260',
+    alt: 'Orange and White Koi Fish Near Yellow Koi Fish',
+  },
+  {
+    url:
+      'https://images.pexels.com/photos/219943/pexels-photo-219943.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260',
+    alt: 'Group of Horses Running',
+  },
+];
 
-const galleryEl = document.querySelector("#gallery");
+const galleryList = document.querySelector('#gallery');
 
-galleryEl.insertAdjacentHTML(
-  "afterbegin",
-  images.reduce((acc, { url, alt }) => {
-    acc += `<li class='gallery-item'><img class='gallery-img' src="${url}" alt = "${alt}"/></li>`;
-    return acc;
-  }, "")
-);
+const makeItemMarkup = ({ url, alt }) => {
+  return `<li class = "gallery-item"><img class = "gallery-img" src = "${url}" alt = "${alt}" width = "350" ></li>`;
+};
+const makeItems = images.map(makeItemMarkup).join('');
+
+galleryList .insertAdjacentHTML('afterbegin', makeItems);
